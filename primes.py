ca09713d6fb33fc437421beab4e454f22ec42fe6
jkurkure <117855264+jkurkure@users.noreply.github.com>
2024-08-29 22:23:47 +0000

    dressing up

--- primes.py
+++ primes.py
@@ -1,13 +1,24 @@
-import math, time, sys, random
+import math, time, sys, random, os
 
-from rich.console import Console
+ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
+BIGNUM = 3**3**8
+
+while True:
+	try:
+		from rich.console import Console
+		from rich.progress import Progress
+		break
+	except ModuleNotFoundError:
+		os.system("pip install rich")
+		continue
+
 console = Console()
 colors = ["red", "green", "yellow", "blue", "magenta", "cyan", "white", "bright_red", "bright_yellow", "indian_red1", "hot_pink", "dark_olive_green3"]
 
 random.seed(time.time() // (60 * 60 * 24))
 
 isPrime = lambda n:[n%i for i in range(1, math.ceil(n/2) + 1)].count(0)==1
-primes = (n for n in range(3**3**8) if isPrime(n))
+primes = (n for n in range(BIGNUM) if isPrime(n))
 
 if __name__ == "__main__":
 	nonInteractive = sys.argv.index("--find") if "--find" in sys.argv else -1
@@ -15,29 +26,35 @@ if __name__ == "__main__":
 		next(primes)
 		index = eval(sys.argv[nonInteractive + 1])
 		random.seed(index)
-		for _ in range(index - 1):
+		for _ in track(range(index - 1), f"Looking for the {ordinal(index)} prime"):
 			next(primes)
 		color = random.choice(colors)
 		console.print(f"[{color}]{next(primes)}[/{color}]")
-		exit()
 
 
 	out = []
 	color = random.choice(colors)
-
 	try:
-		for p in primes:
-			if ("--rainbow" in sys.argv):
-				color = random.choice(colors)
+		with Progress(console=console, auto_refresh=False) as progress:
+			goalpost = 30
+			task = progress.add_task("Hunting primes", total=goalpost)
+			for p in primes:
+				if (goalpost < BIGNUM):
+					goalpost += 5
 
-			out.append(f"[{color}]{p} [/{color}]")
-			
-			if ("--express" not in sys.argv):
-				time.sleep(0.1)
-			
-			if len(out) > 6:
-				console.print("".join(out))
-				out = []
+				if ("--rainbow" in sys.argv):
+					color = random.choice(colors)
+
+				out.append(f"[{color}]{p} [/{color}]")
+				
+				if ("--express" not in sys.argv):
+					time.sleep(0.1)
+				
+				if len(out) > 6:
+					print('\x1b[2k\x1b[100D\x1b[1A')
+					console.print("".join(out))
+					out = []
+					progress.update(task, advance=7, refresh=True, total=goalpost)
 
 	except KeyboardInterrupt:
 		console.print("".join(out))
